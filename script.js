8f8226b688a2ce84fa3435fd590dfa1f2edfb01a
Antonin-lhp5 <totemdebois@gmail.com>
2020-11-02 18:46:45 +0100

    exo js

---
+++ script.js
@@ -0,0 +1,139 @@
+
+// Partie 1 - Les variables et les fonction d'affichage
+
+//  exercice 1
+
+// let message = "Hello world !";
+//     alert(message);
+
+// exercice 2
+
+// let firstPart = "je suis un ";
+// let lastPart = "Chat poulpe !";
+//     alert(firstPart + lastPart)
+
+// exercice 3
+
+// let name = "Patrick ";
+// let age = "88";
+// let city = "la vallée des champignons ";
+
+// alert ("Bonjour, je m'appelle " + name + ", j'ai " + age + " ans " + "et j'habite dans " + city)
+
+// exercice 4
+
+// let userName = prompt("Votre nom")
+// let userAge = prompt("Votre age")
+// let userCity = prompt("Votre ville")
+
+//     alert(" Nom : " + userName + " Âge : " + userAge + " Ville : " + userCity)
+
+// excercice 5
+
+// let number = prompt("nombre1");
+// let number2 = prompt ("nombre2");
+
+// let resulta = parseInt(number) + parseInt(number2)
+
+// alert(number + " + " + number2 + " = " + resulta)
+
+
+// Partie 2 - Les conditions 
+
+// Exercice 1 
+// let number = 20
+
+// if (number > 21) {
+
+//     alert("afficher un message")
+
+// }
+
+// Exercice 2 
+
+// let votreAge = prompt("age")
+
+// let message
+
+// if (votreAge >= 18) {
+
+//     alert("Vous êtes majeurs")
+// }
+
+// else {
+//     alert("Vous êtes mineurs")
+// }
+
+//  Exercice 3
+
+// let nbr = prompt("number")
+
+// if (nbr%2 == 0)
+// {
+//        alert("Nombre pair");
+// }
+// else
+// {
+//        alert("Nombre impair");
+// }
+
+//  Exercice 4
+
+// let nbr = prompt("Mettez une note entre 0 et 10")
+
+// if (nbr <= 3) {
+//     alert("Nul")
+// }
+
+// if (nbr == 10) {
+//     alert("Excellent")
+// }
+
+// if (nbr >= 4 && 6 > nbr )
+//     alert("moyen")
+
+// if (nbr >= 6 && 8 > nbr )
+//     alert("Assez bien")
+
+// if (nbr >= 8 && 9 >= nbr )
+//     alert("Bien")
+
+// Exercice 5
+
+// let day = 4;
+
+switch (day) {
+    case 1:
+        alert('Lundi')
+        break;
+    case 2:
+        alert('Mardi')
+        break;
+    case 3:
+        alert('Mercredi')
+        break;
+    case 4:
+        alert('Jeudi')
+        break;
+    case 5:
+        alert('Vendredi')
+        break;
+    case 6:
+        alert('Samedi')
+        break;
+    case 7:
+        alert('Dimanche')
+        break;
+
+    default:
+        break;
+}
+
+// Partie 3
+
+// Exercice 1
+
+
+
+
+
